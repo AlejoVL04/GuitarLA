f27716895a8f4033f0a9ccb5dc94163f60ae6e86
Alejandro Vélez <alejovelon@gmail.com>
2025-10-15 15:32:42 -0500

    Se agregó homepage en package.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react()],
+  base: '/guitarla/',
   server: {
     port: 3000, // Cambia a un puerto libre
   },
